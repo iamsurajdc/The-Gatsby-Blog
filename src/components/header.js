25cab545a0cdb39bb100a1b61b9df6643fcbef7a
Suraj Chandgude <surajchandgude0304@gmail.com>
2020-09-09 11:36:03 +0530

    📦 NEW: 1) gatsby-plugin-sass Plugin 2) Used SaSS as a css counterpart 3) Some design

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,15 @@
 import React from "react";
 import {Link} from "gatsby";
+import headerStyles from "./header.module.scss"
 
 const Header = () => {
   return (
     <header>
       <h1>The Suraj Chandgude Blog</h1>
-      <span> <Link to="/">Home</Link></span> |
-      <span> <Link to="/about">About</Link></span> |
-      <span> <Link to="/contact">Contact</Link></span> |
-      <span> <Link to="/blog">Blog</Link></span>
+      <span> <Link className={headerStyles.link}  to="/">Home</Link></span> |
+      <span> <Link className={headerStyles.link} to="/about">About</Link></span> |
+      <span> <Link className={headerStyles.link} to="/contact">Contact</Link></span> |
+      <span> <Link className={headerStyles.link} to="/blog">Blog</Link></span>
     </header>
   )
 }
